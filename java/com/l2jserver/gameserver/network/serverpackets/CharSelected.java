fd2cb11d0d479358b7d37c35a77af7797eccd4a2
lineage2kr <lineage2kr@gmail.com>
2013-01-25 12:36:50 +0000

    L2J Server BETA: 프로토콜 440 맞게 패킷 수정.

--- java/com/l2jserver/gameserver/network/serverpackets/CharSelected.java
+++ java/com/l2jserver/gameserver/network/serverpackets/CharSelected.java
@@ -26,10 +26,11 @@ import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
  * @version $Revision: 1.4.2.5.2.6 $ $Date: 2005/03/27 15:29:39 $
  */
 public class CharSelected extends L2GameServerPacket {
+	
 	// SdSddddddddddffddddddddddddddddddddddddddddddddddddddddd d
 	private static final String _S__21_CHARSELECTED = "[S] 0b CharSelected";
-	private L2PcInstance _activeChar;
-	private int _sessionId;
+	private final L2PcInstance _activeChar;
+	private final int _sessionId;
 	
 	/**
 	 * @param cha
@@ -43,30 +44,30 @@ public class CharSelected extends L2GameServerPacket {
 	@SuppressWarnings("deprecation")
 	@Override
 	protected final void writeImpl() {
-		writeC(0x0b);
+		writeC(11);
 		
 		writeS(_activeChar.getName());
-		writeD(_activeChar.getCharId()); // ??
+		writeD(_activeChar.getCharId());
 		writeS(_activeChar.getTitle());
 		writeD(_sessionId);
 		writeD(_activeChar.getClanId());
-		writeD(0x00); // ??
+		writeD(0); // ??
 		writeD(_activeChar.getAppearance().getSex() ? 1 : 0);
 		writeD(_activeChar.getRace().ordinal());
 		writeD(_activeChar.getClassId().getId());
-		writeD(0x01); // active ??
+		writeD(1);  // active ??
 		writeD(_activeChar.getX());
 		writeD(_activeChar.getY());
 		writeD(_activeChar.getZ());
-		
 		writeF(_activeChar.getCurrentHp());
 		writeF(_activeChar.getCurrentMp());
 		writeD(_activeChar.getSp());
 		writeQ(_activeChar.getExp());
 		writeD(_activeChar.getLevel());
 		int Karma = _activeChar.getKarma(); // rocknow-God-Test
-		if (Karma > 0) // rocknow-God-Test
+		if (Karma > 0) { // rocknow-God-Test
 			Karma = 0 - Karma; // rocknow-God-Test
+		}
 		writeD(Karma); // thx evill33t //rocknow-God-Test
 		writeD(_activeChar.getPkKills());
 		writeD(_activeChar.getINT());
@@ -76,22 +77,23 @@ public class CharSelected extends L2GameServerPacket {
 		writeD(_activeChar.getDEX());
 		writeD(_activeChar.getWIT());
 		
-		writeD(GameTimeController.getInstance().getGameTime() % (24 * 60)); // "reset" on 24th hour
-		writeD(0x00);
+		writeD(GameTimeController.getInstance().getGameTime() % 1440); // "reset" on 24th hour
+		writeD(0);
 		
 		writeD(_activeChar.getClassId().getId());
 		
-		writeD(0x00);
-		writeD(0x00);
-		writeD(0x00);
-		writeD(0x00);
+		writeD(0);
+		writeD(0);
+		writeD(0);
+		writeD(0);
 		
 		writeB(new byte[64]);
-		writeD(0x00);
+		writeD(0);
 	}
 	
 	@Override
 	public String getType() {
 		return _S__21_CHARSELECTED;
 	}
+	
 }
