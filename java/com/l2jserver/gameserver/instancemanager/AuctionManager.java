87bbe4382de095d3a87df6975126e5c6c5d2c929
lineage2kr <lineage2kr@gmail.com>
2013-02-13 11:41:01 +0000

    L2J Server, DataPack BETA: 아지트 관련 한글화

--- java/com/l2jserver/gameserver/instancemanager/AuctionManager.java
+++ java/com/l2jserver/gameserver/instancemanager/AuctionManager.java
@@ -30,51 +30,56 @@ import javolution.util.FastList;
 import com.l2jserver.L2DatabaseFactory;
 import com.l2jserver.gameserver.model.entity.Auction;
 
+/**
+ * 경매 관리
+ * @author lineage2kr
+ */
 public class AuctionManager {
 	
 	protected static final Logger _log = Logger.getLogger(AuctionManager.class.getName());
+	
 	private final List<Auction> _auctions;
 	
 	private static final String[] ITEM_INIT_DATA =
 	{
-		"(22, 0, 'NPC', 'NPC Clan', 'ClanHall', 22, 0, 'Moonstone Hall', 1, 20000000, 0, 1164841200000)",
-		"(23, 0, 'NPC', 'NPC Clan', 'ClanHall', 23, 0, 'Onyx Hall', 1, 20000000, 0, 1164841200000)",
-		"(24, 0, 'NPC', 'NPC Clan', 'ClanHall', 24, 0, 'Topaz Hall', 1, 20000000, 0, 1164841200000)",
-		"(25, 0, 'NPC', 'NPC Clan', 'ClanHall', 25, 0, 'Ruby Hall', 1, 20000000, 0, 1164841200000)",
-		"(26, 0, 'NPC', 'NPC Clan', 'ClanHall', 26, 0, 'Crystal Hall', 1, 20000000, 0, 1164841200000)",
-		"(27, 0, 'NPC', 'NPC Clan', 'ClanHall', 27, 0, 'Onyx Hall', 1, 20000000, 0, 1164841200000)",
-		"(28, 0, 'NPC', 'NPC Clan', 'ClanHall', 28, 0, 'Sapphire Hall', 1, 20000000, 0, 1164841200000)",
-		"(29, 0, 'NPC', 'NPC Clan', 'ClanHall', 29, 0, 'Moonstone Hall', 1, 20000000, 0, 1164841200000)",
-		"(30, 0, 'NPC', 'NPC Clan', 'ClanHall', 30, 0, 'Emerald Hall', 1, 20000000, 0, 1164841200000)",
-		"(31, 0, 'NPC', 'NPC Clan', 'ClanHall', 31, 0, 'The Atramental Barracks', 1, 8000000, 0, 1164841200000)",
-		"(32, 0, 'NPC', 'NPC Clan', 'ClanHall', 32, 0, 'The Scarlet Barracks', 1, 8000000, 0, 1164841200000)",
-		"(33, 0, 'NPC', 'NPC Clan', 'ClanHall', 33, 0, 'The Viridian Barracks', 1, 8000000, 0, 1164841200000)",
-		"(36, 0, 'NPC', 'NPC Clan', 'ClanHall', 36, 0, 'The Golden Chamber', 1, 50000000, 0, 1164841200000)",
-		"(37, 0, 'NPC', 'NPC Clan', 'ClanHall', 37, 0, 'The Silver Chamber', 1, 50000000, 0, 1164841200000)",
-		"(38, 0, 'NPC', 'NPC Clan', 'ClanHall', 38, 0, 'The Mithril Chamber', 1, 50000000, 0, 1164841200000)",
-		"(39, 0, 'NPC', 'NPC Clan', 'ClanHall', 39, 0, 'Silver Manor', 1, 50000000, 0, 1164841200000)",
-		"(40, 0, 'NPC', 'NPC Clan', 'ClanHall', 40, 0, 'Gold Manor', 1, 50000000, 0, 1164841200000)",
-		"(41, 0, 'NPC', 'NPC Clan', 'ClanHall', 41, 0, 'The Bronze Chamber', 1, 50000000, 0, 1164841200000)",
-		"(42, 0, 'NPC', 'NPC Clan', 'ClanHall', 42, 0, 'The Golden Chamber', 1, 50000000, 0, 1164841200000)",
-		"(43, 0, 'NPC', 'NPC Clan', 'ClanHall', 43, 0, 'The Silver Chamber', 1, 50000000, 0, 1164841200000)",
-		"(44, 0, 'NPC', 'NPC Clan', 'ClanHall', 44, 0, 'The Mithril Chamber', 1, 50000000, 0, 1164841200000)",
-		"(45, 0, 'NPC', 'NPC Clan', 'ClanHall', 45, 0, 'The Bronze Chamber', 1, 50000000, 0, 1164841200000)",
-		"(46, 0, 'NPC', 'NPC Clan', 'ClanHall', 46, 0, 'Silver Manor', 1, 50000000, 0, 1164841200000)",
-		"(47, 0, 'NPC', 'NPC Clan', 'ClanHall', 47, 0, 'Moonstone Hall', 1, 50000000, 0, 1164841200000)",
-		"(48, 0, 'NPC', 'NPC Clan', 'ClanHall', 48, 0, 'Onyx Hall', 1, 50000000, 0, 1164841200000)",
-		"(49, 0, 'NPC', 'NPC Clan', 'ClanHall', 49, 0, 'Emerald Hall', 1, 50000000, 0, 1164841200000)",
-		"(50, 0, 'NPC', 'NPC Clan', 'ClanHall', 50, 0, 'Sapphire Hall', 1, 50000000, 0, 1164841200000)",
-		"(51, 0, 'NPC', 'NPC Clan', 'ClanHall', 51, 0, 'Mont Chamber', 1, 50000000, 0, 1164841200000)",
-		"(52, 0, 'NPC', 'NPC Clan', 'ClanHall', 52, 0, 'Astaire Chamber', 1, 50000000, 0, 1164841200000)",
-		"(53, 0, 'NPC', 'NPC Clan', 'ClanHall', 53, 0, 'Aria Chamber', 1, 50000000, 0, 1164841200000)",
-		"(54, 0, 'NPC', 'NPC Clan', 'ClanHall', 54, 0, 'Yiana Chamber', 1, 50000000, 0, 1164841200000)",
-		"(55, 0, 'NPC', 'NPC Clan', 'ClanHall', 55, 0, 'Roien Chamber', 1, 50000000, 0, 1164841200000)",
-		"(56, 0, 'NPC', 'NPC Clan', 'ClanHall', 56, 0, 'Luna Chamber', 1, 50000000, 0, 1164841200000)",
-		"(57, 0, 'NPC', 'NPC Clan', 'ClanHall', 57, 0, 'Traban Chamber', 1, 50000000, 0, 1164841200000)",
-		"(58, 0, 'NPC', 'NPC Clan', 'ClanHall', 58, 0, 'Eisen Hall', 1, 50000000, 0, 1164841200000)",
-		"(59, 0, 'NPC', 'NPC Clan', 'ClanHall', 59, 0, 'Heavy Metal Hall', 1, 50000000, 0, 1164841200000)",
-		"(60, 0, 'NPC', 'NPC Clan', 'ClanHall', 60, 0, 'Molten Ore Hall', 1, 50000000, 0, 1164841200000)",
-		"(61, 0, 'NPC', 'NPC Clan', 'ClanHall', 61, 0, 'Titan Hall', 1, 50000000, 0, 1164841200000)"
+		"(22, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 22, 0, '문스톤 홀', 1, 20000000, 0, 1164841200000)",
+		"(23, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 23, 0, '오닉스 홀', 1, 20000000, 0, 1164841200000)",
+		"(24, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 24, 0, '토파즈 홀', 1, 20000000, 0, 1164841200000)",
+		"(25, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 25, 0, '루비 홀', 1, 20000000, 0, 1164841200000)",
+		"(26, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 26, 0, '크리스탈 홀', 1, 20000000, 0, 1164841200000)",
+		"(27, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 27, 0, '오닉스 홀', 1, 20000000, 0, 1164841200000)",
+		"(28, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 28, 0, '사파이어 홀', 1, 20000000, 0, 1164841200000)",
+		"(29, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 29, 0, '문스톤 홀', 1, 20000000, 0, 1164841200000)",
+		"(30, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 30, 0, '에메랄드 홀', 1, 20000000, 0, 1164841200000)",
+		"(31, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 31, 0, '블랙 배럭스', 1, 8000000, 0, 1164841200000)",
+		"(32, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 32, 0, '레드 배럭스', 1, 8000000, 0, 1164841200000)",
+		"(33, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 33, 0, '그린 배럭스', 1, 8000000, 0, 1164841200000)",
+		"(36, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 36, 0, '골든 라운지', 1, 50000000, 0, 1164841200000)",
+		"(37, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 37, 0, '실버 라운지', 1, 50000000, 0, 1164841200000)",
+		"(38, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 38, 0, '미스릴 라운지', 1, 50000000, 0, 1164841200000)",
+		"(39, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 39, 0, '실버 하우스', 1, 50000000, 0, 1164841200000)",
+		"(40, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 40, 0, '골든 하우스', 1, 50000000, 0, 1164841200000)",
+		"(41, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 41, 0, '브론즈 라운지', 1, 50000000, 0, 1164841200000)",
+		"(42, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 42, 0, '골든 라운지', 1, 50000000, 0, 1164841200000)",
+		"(43, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 43, 0, '실버 라운지', 1, 50000000, 0, 1164841200000)",
+		"(44, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 44, 0, '미스릴 라운지', 1, 50000000, 0, 1164841200000)",
+		"(45, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 45, 0, '브론즈 라운지', 1, 50000000, 0, 1164841200000)",
+		"(46, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 46, 0, '실버 하우스', 1, 50000000, 0, 1164841200000)",
+		"(47, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 47, 0, '문스톤 홀', 1, 50000000, 0, 1164841200000)",
+		"(48, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 48, 0, '오닉스 홀', 1, 50000000, 0, 1164841200000)",
+		"(49, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 49, 0, '에메랄드 홀', 1, 50000000, 0, 1164841200000)",
+		"(50, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 50, 0, '사파이어 홀', 1, 50000000, 0, 1164841200000)",
+		"(51, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 51, 0, '몬트 라운지', 1, 50000000, 0, 1164841200000)",
+		"(52, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 52, 0, '슈테른 라운지', 1, 50000000, 0, 1164841200000)",
+		"(53, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 53, 0, '베누스 라운지', 1, 50000000, 0, 1164841200000)",
+		"(54, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 54, 0, '마르스 라운지', 1, 50000000, 0, 1164841200000)",
+		"(55, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 55, 0, '아데스 라운지', 1, 50000000, 0, 1164841200000)",
+		"(56, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 56, 0, '프루토 라운지', 1, 50000000, 0, 1164841200000)",
+		"(57, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 57, 0, '트라반 라운지', 1, 50000000, 0, 1164841200000)",
+		"(58, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 58, 0, '아이젠 홀', 1, 50000000, 0, 1164841200000)",
+		"(59, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 59, 0, '베어메탈 홀', 1, 50000000, 0, 1164841200000)",
+		"(60, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 60, 0, '히트메탈 홀', 1, 50000000, 0, 1164841200000)",
+		"(61, 0, 'NPC', '혈맹 NPC', '혈맹 아지트', 61, 0, '티탄 홀', 1, 50000000, 0, 1164841200000)"
 	};
 	
 	private static final int[] ItemInitDataId =
@@ -133,25 +138,32 @@ public class AuctionManager {
 		load();
 	}
 	
+	/**
+	 * 경매 아지트 번호 로드.
+	 */
 	private final void load() {
 		Connection con = null;
 		try {
 			con = L2DatabaseFactory.getInstance().getConnection();
-			PreparedStatement statement = con.prepareStatement("SELECT id FROM auction ORDER BY id");
+			PreparedStatement statement = con.prepareStatement("SELECT `id` FROM `auction` ORDER BY `id`");
 			ResultSet rs = statement.executeQuery();
 			while (rs.next()) {
 				_auctions.add(new Auction(rs.getInt("id")));
 			}
 			rs.close();
 			statement.close();
-			_log.info("Loaded: " + getAuctions().size() + " auction(s)");
+			_log.info(getAuctions().size() + "개 경매가 로드되었습니다.");
 		} catch (Exception e) {
-			_log.log(Level.WARNING, "Exception: AuctionManager.load(): " + e.getMessage(), e);
+			_log.log(Level.WARNING, "예외: AuctionManager.load(): " + e.getMessage(), e);
 		} finally {
 			L2DatabaseFactory.close(con);
 		}
 	}
 	
+	/**
+	 * @param auctionId
+	 * @return
+	 */
 	public final Auction getAuction(int auctionId) {
 		int index = getAuctionIndex(auctionId);
 		if (index >= 0) {
@@ -160,6 +172,10 @@ public class AuctionManager {
 		return null;
 	}
 	
+	/**
+	 * @param auctionId
+	 * @return
+	 */
 	public final int getAuctionIndex(int auctionId) {
 		Auction auction;
 		for (int i = 0; i < getAuctions().size(); i++) {
@@ -171,12 +187,15 @@ public class AuctionManager {
 		return -1;
 	}
 	
+	/**
+	 * @return
+	 */
 	public final List<Auction> getAuctions() {
 		return _auctions;
 	}
 	
 	/**
-	 * Init Clan NPC aution
+	 * 경매 초기 혈맹 NPC
 	 * @param id
 	 */
 	public void initNPC(int id) {
@@ -187,7 +206,7 @@ public class AuctionManager {
 			}
 		}
 		if ((i >= ItemInitDataId.length) || (ItemInitDataId[i] != id)) {
-			_log.warning("Clan Hall auction not found for Id :" + id);
+			_log.warning("아지트 경매를 찾을 수 없습니다. ID:" + id);
 			return;
 		}
 		
@@ -198,7 +217,7 @@ public class AuctionManager {
 			statement.execute();
 			statement.close();
 			_auctions.add(new Auction(id));
-			_log.info("Created auction for ClanHall: " + id);
+			_log.info("아지트 경매가 생성되었습니다. ID: " + id);
 		} catch (Exception e) {
 			_log.log(Level.SEVERE, "Exception: Auction.initNPC(): " + e.getMessage(), e);
 		} finally {
