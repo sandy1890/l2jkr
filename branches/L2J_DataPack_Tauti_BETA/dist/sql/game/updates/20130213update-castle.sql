78c43992989c9e160d5237e58244cc57bbcb325c
lineage2kr@gmail.com <lineage2kr@gmail.com@72565907-0f3c-e3de-fd5f-e4bed459a0a1>
2013-02-13 12:12:22 +0000

    L2J DataPack BETA: 성 이름 SQL 데이터 한글화

---
+++ branches/L2J_DataPack_Tauti_BETA/dist/sql/game/updates/20130213update-castle.sql
@@ -0,0 +1,13 @@
+SET NAMES UTF8;
+
+UPDATE `castle` SET `name` = '글루디오' WHERE `id` = 1;
+UPDATE `castle` SET `name` = '디온' WHERE `id` = 2;
+UPDATE `castle` SET `name` = '기란' WHERE `id` = 3;
+UPDATE `castle` SET `name` = '오렌' WHERE `id` = 4;
+UPDATE `castle` SET `name` = '아덴' WHERE `id` = 5;
+UPDATE `castle` SET `name` = '인나드릴' WHERE `id` = 6;
+UPDATE `castle` SET `name` = '고다드' WHERE `id` = 7;
+UPDATE `castle` SET `name` = '루운' WHERE `id` = 8;
+UPDATE `castle` SET `name` = '슈트가르트' WHERE `id` = 9;
+
+COMMIT;
