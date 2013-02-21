6c8a9882af1ceeaf0703577dcfc07ddaa19d3ee7
lineage2kr <lineage2kr@gmail.com>
2013-02-21 21:26:57 +0000

    L2J DataPack BETA: 게이트키퍼 생일 도우미 호출 안되던 문제 수정.

--- dist/game/data/scripts/events/CharacterBirthday/CharacterBirthday.java
+++ dist/game/data/scripts/events/CharacterBirthday/CharacterBirthday.java
@@ -32,47 +32,32 @@ import com.l2jserver.gameserver.util.Util;
  */
 public class CharacterBirthday extends Quest {
 	
-	private static final int NPC = 32600; // 알레그리아
+	private static final int ALEGRIA = 32600; // 알레그리아
 	
 	private static int SPAWNS = 0;
 	
-	private final static int[] GATE_KEEPER =
+	private final static int[] GATEKEEPER =
 	{
 		30006, // 밀리아 - 말하는 섬 마을 게이트키퍼
-		30059, // 트리시아
-		30080, // 크라비아
-		30134, // 텔레포트 장치
-		30146, // 텔레포트 장치
-		30177, // 발렌티나
-		30233, // 에스메랄다
-		30256, // 베라돈나
-		30320, // 리클린
-		30540, // 텔레포트 장치
-		30576, // 텔레포트 장치
-		30836, // 미네베아
-		30848, // 엘리자베스
-		30878, // 안젤리나
-		30899, // 플라우엔
-		31275, // 타티아나
-		31320, // 일리야나
-		31964, // 빌리아
-		32163, // 텔레포트 장치
+		30059, // 트리시아 - 디온성 마을 게이트키퍼
+		30080, // 크라비아 - 기란성 마을 게이트키퍼
+		30134, // 텔레포트 장치 - 다크엘프 마을
+		30146, // 텔레포트 장치 - 엘프 마을
+		30177, // 발렌티나 - 오렌성 마을 게이트키퍼
+		30233, // 에스메랄다 - 사냥꾼 마을 게이트키퍼
+		30256, // 베라돈나 - 글루디오성 마을 게이트키퍼
+		30320, // 리클린 - 글루딘 마을 게이트키퍼
+		30540, // 텔레포트 장치 - 드워프 마을
+		30576, // 텔레포트 장치 - 오크 마을
+		30836, // 미네베아 - 하딘의 사숙
+		30848, // 엘리자베스 - 아덴성 마을 게이트키퍼
+		30878, // 안젤리나 - 기란 항구
+		30899, // 플라우엔 - 하이네스 마을 게이트키퍼
+		31275, // 타티아나 - 고다드성 마을 게이트키퍼
+		31320, // 일리야나 - 루운성 마을 게이트키퍼
+		31964, // 빌리아 - 슈트가르트성 마을 게이트키퍼
+		32163, // 텔레포트 장치 - 카마엘 마을
 	};
-	
-	/**
-	 * @param questId
-	 * @param name
-	 * @param descr
-	 */
-	public CharacterBirthday(int questId, String name, String descr) {
-		super(questId, name, descr);
-		addStartNpc(NPC);
-		addTalkId(NPC);
-		for (int id : GATE_KEEPER) {
-			addStartNpc(id);
-			addTalkId(id);
-		}
-	}
 	
 	@Override
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player) {
@@ -108,7 +93,7 @@ public class CharacterBirthday extends Quest {
 			st = newQuestState(player);
 		}
 		if (!Util.checkIfInRange(10, npc, player, true)) {
-			L2Npc spawned = st.addSpawn(32600, player.getX() + 10, player.getY() + 10, player.getZ() + 10, 0, false, 0, true);
+			L2Npc spawned = st.addSpawn(ALEGRIA, player.getX() + 10, player.getY() + 10, player.getZ() + 10, 0, false, 0, true);
 			player.sendPacket(new PlaySound(1, "HB01", 0, 0, 0, 0, 0));
 			st.setState(State.STARTED);
 			st.startQuestTimer("despawn_npc", 180000, spawned);
@@ -119,8 +104,21 @@ public class CharacterBirthday extends Quest {
 		return null;
 	}
 	
+	/**
+	 * @param questId
+	 * @param name
+	 * @param descr
+	 */
+	public CharacterBirthday(int questId, String name, String descr) {
+		super(questId, name, descr);
+		addStartNpc(ALEGRIA);
+		addStartNpc(GATEKEEPER);
+		addTalkId(ALEGRIA);
+		addTalkId(GATEKEEPER);
+	}
+	
 	public static void main(String[] args) {
-		new CharacterBirthday(-1, CharacterBirthday.class.getName(), "events");
+		new CharacterBirthday(-1, CharacterBirthday.class.getSimpleName(), "events");
 	}
 	
 }
